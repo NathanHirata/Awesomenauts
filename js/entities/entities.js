46616b322ae470ff1167f53990c0d242732c9bb2
Nathan Hirata <nathanhirata@yahoo.com>
2015-03-10 21:22:16 -0700

    vid 10

--- js/entities/entities.js
+++ js/entities/entities.js
@@ -10,9 +10,35 @@ game.PlayerEntity = me.Entity.extend({
                     return(new me.Rect(0, 0, 64, 64)).toPolygon();
                 }
         }]);
+    
+    this.body.setVelocity(5, 20);
+    
+    this.renderable.addAnimation("idle", [78]);
+    this.renderable.addAnimation("walk", [117,118,119,120,121,122,123,124,125], 80);
+    
+    this.renderable.setCurrentAnimation("idle");
     },
     
-    update: function() {
+    update: function(delta) {
+        if(me.input.isKeyPressed("right")) {
+            this.body.vel.x += this.body.accel.x * me.timer.tick;
+            this.flipX(true);
+        }else{
+            this.body.vel.x = 0;
+        }
         
+        if(this.body.vel.x !== 0){
+        if(!this.renderable.isCurrentAnimation("walk")) {
+            this.renderable.setCurrentAnimation("walk");
+        }
+        else{
+            this.renderable.setCurrentAnimation("idle");
+        }
+        }
+        
+        this.body.update(delta);
+        
+        this._super(me.Entity, "update", [delta]);
+        return true;
     }
 });
