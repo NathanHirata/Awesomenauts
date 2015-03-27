3c36539e81a430df43b4016f3fae6cfdc32e09fe
Nathan Hirata <nathanhirata@yahoo.com>
2015-03-27 13:41:42 -0700

    idk

--- js/game.js
+++ js/game.js
@@ -42,6 +42,7 @@ var game = {
             me.pool.register("player", game.PlayerEntity, true);
             me.pool.register("enemyBase", game.EnemyBaseEntity);
             me.pool.register("playerBase", game.PlayerBaseEntity);
+            me.pool.register("EnemyCreep", game.EnemyCreep);
             
 		me.state.set(me.state.MENU, new game.TitleScreen());
 		me.state.set(me.state.PLAY, new game.PlayScreen());
