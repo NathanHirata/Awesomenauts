c0cab176b36c02c4c9cc0a8eb5896ed512b89d2d
Nathan Hirata <nathanhirata@yahoo.com>
2015-04-02 21:09:55 -0700

    23

--- js/entities/entities.js
+++ js/entities/entities.js
@@ -91,9 +91,32 @@ game.PlayerEntity = me.Entity.extend({
                 this.pos.x = this.pos.x +1;
             }
             
-            if(this.renderable.isCurrentAnimation("attack") && this.now-this.lastHit >= 400){
+            if(this.renderable.isCurrentAnimation("attack") && this.now-this.lastHit >= 1000){
             this.lastHit = this.now;
             response.b.loseHealth();
+            }
+        }else if(response.b.type==='EnemyCreep ') {
+            var xdif = this.pos.x - response.b.pos.x;
+            var ydif = this.pos.y - response.b.pos.y;
+            
+            if(xdif>0){
+                this.pos.x = this.pos.x + 1;
+                if(this.facing==="left"){
+                    this.body.vel.x = 0;
+                }
+            }else{
+                if(this.facing==="right"){
+                    this.body.vel.x = 0;
+                }
+                this.pos.x = this.pos.x - 1;
+            }
+            
+            if(this.renderable.isCurrentAnimation("attack") && this.now-this.lastHit >= 400
+                    && (Math.abs(ydif)<=40) && 
+                    (((xdif>0 ) && this.facing==="left")  || ((xdif<0) && this.facing==="right"))
+                    ){
+                this.lastHit = this.now;
+                response.b.loseHealth(1);
             }
         }
     }
